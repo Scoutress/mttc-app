a1aa3841a3e2f55d9b1903c71e71212a9c364285
Scoutress <142579732+Scoutress@users.noreply.github.com>
2025-05-04 20:39:23 +0300

    Updated personal storage data

--- src/storage/PersonalStorage.js
+++ src/storage/PersonalStorage.js
@@ -1,8 +1,28 @@
 const PersonalStorage = [
   {
-    code: "GH96-15528A",
-    desc: "Desc",
-    dateTaken: "2025-05-01",
+    code: "GH82-34224A",
+    desc: "OLED Assembly Without Frame",
+    dateTaken: "2025-05-04",
+  },
+  {
+    code: "GH82-34228A",
+    desc: "Battery",
+    dateTaken: "2025-05-04",
+  },
+  {
+    code: "GH96-16719A",
+    desc: "USB Sub Board",
+    dateTaken: "2025-05-04",
+  },
+  {
+    code: "GH96-16675B",
+    desc: "Back Camera Wide",
+    dateTaken: "2025-05-04",
+  },
+  {
+    code: "GH98-48993A",
+    desc: "Dual SIM Tray Black",
+    dateTaken: "2025-05-04",
   },
 ];
 
