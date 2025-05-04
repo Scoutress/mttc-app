0de947332954d3ddba6ca910a9ad28874083f7fa
Scoutress <142579732+Scoutress@users.noreply.github.com>
2025-05-04 20:22:20 +0300

    Updated possible actions

--- src/storage/Actions.js
+++ src/storage/Actions.js
@@ -2,14 +2,77 @@ const Actions = [
   {
     code: "PRT",
     description: "Reikalingų detalių užsakymas",
+    time: 5,
   },
   {
-    code: "DIS",
-    description: "Prietaisas išardytas",
+    code: "DOP",
+    description: "Galinio dangtelio nukėlimas",
+    time: 7,
   },
   {
-    code: "REP",
-    description: "Ekranas pakeistas",
+    code: "OCT",
+    description: "Galinio ekrano nukėlimas",
+    time: 8,
+  },
+  {
+    code: "DDA",
+    description: "Išardymas",
+    time: 12,
+  },
+  {
+    code: "DAS",
+    description: "Surinkimas",
+    time: 14,
+  },
+  {
+    code: "PRS",
+    description: "Galinio dangtelio prispaudimas presu",
+    time: 4,
+  },
+  {
+    code: "PTS",
+    description: "Įrenginio ištestavimas",
+    time: 10,
+  },
+  {
+    code: "FRE",
+    description: "Įrenginio atrakinimas",
+    time: 6,
+  },
+  {
+    code: "GDT",
+    description: "Diagnostikos atlikimas",
+    time: 15,
+  },
+  {
+    code: "IMW",
+    description: "IMEI įrašymas",
+    time: 3,
+  },
+  {
+    code: "SWU",
+    description: "Programinės įrangos atnaujinimas",
+    time: 20,
+  },
+  {
+    code: "CLN",
+    description: "Sudėtingas valymas",
+    time: 9,
+  },
+  {
+    code: "RAS",
+    description: "Perrinkimas",
+    time: 11,
+  },
+  {
+    code: "PRR",
+    description: "Detalių paruošimas surinkimui",
+    time: 6,
+  },
+  {
+    code: "BTR",
+    description: "Baterijos atskyrimas nuo įrenginio korpuso",
+    time: 7,
   },
 ];
 
