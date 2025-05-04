3b7e60a77e39e4d71f13f704678698624592edbc
Scoutress <142579732+Scoutress@users.noreply.github.com>
2025-05-04 20:43:17 +0300

    Updated warehouse data

--- src/storage/Warehouse.js
+++ src/storage/Warehouse.js
@@ -1,16 +1,24 @@
 const Warehouse = [
-  {
-    code: "GH96-15528A",
-    quantity: 10,
-  },
-  {
-    code: "GH82-12345B",
-    quantity: 0,
-  },
-  {
-    code: "GH43-98765C",
-    quantity: 3,
-  },
+  { code: "GH82-34224A", quantity: 73 },
+  { code: "GH82-34225A", quantity: 11 },
+  { code: "GH82-34225B", quantity: 54 },
+  { code: "GH82-34225D", quantity: 29 },
+  { code: "GH82-34228A", quantity: 38 },
+  { code: "GH96-16719A", quantity: 4 },
+  { code: "GH82-34227A", quantity: 17 },
+  { code: "GH96-16675B", quantity: 55 },
+  { code: "GH96-16673A", quantity: 60 },
+  { code: "GH96-16604A", quantity: 14 },
+  { code: "GH96-16960A", quantity: 75 },
+  { code: "GH02-25711A", quantity: 63 },
+  { code: "GH02-25802A", quantity: 72 },
+  { code: "GH81-25066A", quantity: 67 },
+  { code: "GH81-23484A", quantity: 65 },
+  { code: "GH98-48993A", quantity: 74 },
+  { code: "GH98-48993B", quantity: 32 },
+  { code: "GH98-48993D", quantity: 53 },
+  { code: "GH98-49048B", quantity: 13 },
+  { code: "GH98-49048D", quantity: 50 },
 ];
 
 export default Warehouse;
